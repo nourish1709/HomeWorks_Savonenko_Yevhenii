6e2a1bca74bf7a2f5aa9d50a0d9d2073af7aee97
Zhenya <zhenya.savonenko944@gmail.com>
2020-12-02 17:09:46 +0200

    MyList completes the first task. For the first task a custom Comparator (class NumbersComparator) was added

--- src/homework5/MyList.java
+++ src/homework5/MyList.java
@@ -1,5 +1,34 @@
 package homework5;
 
-public class MyList<T extends Number & Comparable<T>> extends MyListModified<T> {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class MyList<T extends Number> {
+    protected final List<T> tArrayList = new ArrayList<>();
+
+    public void add(T t){
+        this.tArrayList.add(t);
+    }
+
+    public List<T> getArrayList(){
+        return tArrayList;
+    }
+
+    public T getLargest(){
+        return Collections.max(tArrayList, new NumbersComparator());
+    }
+
+    public T getSmallest(){
+        return Collections.min(tArrayList, new NumbersComparator());
+    }
+
+    @Override
+    public String toString() {
+        System.out.print("ArrayList from generic class: \n\t");
+        for(T t : this.tArrayList) {
+            System.out.print(t + " ");
+        }
+        return "\n";
+    }
 }
